685bea4251b1e4824af2de89bdf27f0af9628d9e
pyncz <pavelyanycz@gmail.com>
2023-10-01 22:23:06 +0300

    feat(client): Use `--name` as a prover argument

--- client/index.js
+++ client/index.js
@@ -1,11 +1,19 @@
 const axios = require('axios');
+const minimist = require('minimist');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-const name = 'Jeremy Hansen III' // correct one
-// const name = 'Jeremy Hansen IV' // wrong one
+// read name arg
+const name = minimist(process.argv.slice(2)).name;
+// const name = 'Jeremy Hansen III'; // correct one
+// const name = 'Jeremy Hansen IV'; // wrong one
+
+if (!name) {
+  console.error('Provide the name first!');
+  process.exit(1)
+}
 
 async function main() {
   // get proof we're on the list
